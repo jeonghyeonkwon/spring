afb301d0defade091a014af69c1bd3edfe33a658
jeonghyeonkwon <givejeong2468@gmail.com>
2022-10-27 16:12:12 +0900

    스프링 mvc 연동 정리

--- src/main/java/com/jeonghyeon/study/README.md
+++ src/main/java/com/jeonghyeon/study/README.md
@@ -11,6 +11,17 @@
 
 ### 주요 키워드(설명 추가 예정)
 * RootApplicationContext
+  * Web에 관련된 것은 없다 (Service, Repositorys)
 * WebApplicationContext
   * Servlet WebApplicationContext
+    * RootApplicationContext를 상속 받아 만든 것이다 
+    * DispatcherServlet에 의해 만들어 준다
+    * Controller, ViewResolver, HandlerMapping이 있다.
 * ContextLoaderListener
+
+
+### Spring VS SpringBoot
+* 스프링
+  * 톰캣안에 스프링을 넣은 형태 (서블릿 컨텍스트 안에 스프링이 들어간 구조)
+* 스프링 부트
+  * 스프링 부트 어플리케이션 안에 톰캣을 넣은 구조 (임베디드 톰캣)
--- src/main/java/com/jeonghyeon/study/WebConfig.java
+++ src/main/java/com/jeonghyeon/study/WebConfig.java
@@ -4,7 +4,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 
-//@Configuration
-//@ComponentScan(excludeFilters = @ComponentScan.Filter(Controller.class))
-public class AppConfig {
+@Configuration
+//@ComponentScan(useDefaultFilters = false,includeFilters = @ComponentScan.Filter(Controller.class))
+@ComponentScan
+public class WebConfig {
 }
